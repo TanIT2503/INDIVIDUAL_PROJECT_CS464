a9982162a5f7e78b6e5f79d1b24c5594d6e263dc
kim302 <kimnganit2001@gmail.com>
2022-07-26 12:17:13 +0700

    quanlykhachhang

---
+++ INDIVIDUAL_PROJECT/QuanLyKhachHang.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace INDIVIDUAL_PROJECT
+{
+    partial class QuanLyKhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.btn_Sua = new System.Windows.Forms.Button();
+            this.btn_Xoa = new System.Windows.Forms.Button();
+            this.btn_Them = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_TimKiem = new System.Windows.Forms.TextBox();
+            this.cb_Search = new System.Windows.Forms.ComboBox();
+            this.dt_NgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.cb_LoaiKH = new System.Windows.Forms.ComboBox();
+            this.cb_GioiTinh = new System.Windows.Forms.ComboBox();
+            this.txt_DiaChi = new System.Windows.Forms.TextBox();
+            this.txt_SoKH = new System.Windows.Forms.TextBox();
+            this.txt_TenKH = new System.Windows.Forms.TextBox();
+            this.txt_IDKH = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lb_SoKH = new System.Windows.Forms.Label();
+            this.lb_NgaySinh = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.data_DanhSachKH = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.data_DanhSachKH)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Variable Display", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(375, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "QUẢN LÝ KHÁCH HÀNG ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Clear);
+            this.groupBox1.Controls.Add(this.btn_Sua);
+            this.groupBox1.Controls.Add(this.btn_Xoa);
+            this.groupBox1.Controls.Add(this.btn_Them);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txt_TimKiem);
+            this.groupBox1.Controls.Add(this.cb_Search);
+            this.groupBox1.Controls.Add(this.dt_NgaySinh);
+            this.groupBox1.Controls.Add(this.cb_LoaiKH);
+            this.groupBox1.Controls.Add(this.cb_GioiTinh);
+            this.groupBox1.Controls.Add(this.txt_DiaChi);
+            this.groupBox1.Controls.Add(this.txt_SoKH);
+            this.groupBox1.Controls.Add(this.txt_TenKH);
+            this.groupBox1.Controls.Add(this.txt_IDKH);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.lb_SoKH);
+            this.groupBox1.Controls.Add(this.lb_NgaySinh);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1037, 306);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhập thông tin";
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btn_Clear.Location = new System.Drawing.Point(898, 269);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(88, 23);
+            this.btn_Clear.TabIndex = 21;
+            this.btn_Clear.Text = "CLEAR";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            // 
+            // btn_Sua
+            // 
+            this.btn_Sua.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btn_Sua.Location = new System.Drawing.Point(778, 269);
+            this.btn_Sua.Name = "btn_Sua";
+            this.btn_Sua.Size = new System.Drawing.Size(75, 23);
+            this.btn_Sua.TabIndex = 20;
+            this.btn_Sua.Text = "SỬA";
+            this.btn_Sua.UseVisualStyleBackColor = true;
+            // 
+            // btn_Xoa
+            // 
+            this.btn_Xoa.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btn_Xoa.Location = new System.Drawing.Point(650, 269);
+            this.btn_Xoa.Name = "btn_Xoa";
+            this.btn_Xoa.Size = new System.Drawing.Size(75, 23);
+            this.btn_Xoa.TabIndex = 19;
+            this.btn_Xoa.Text = "XOÁ";
+            this.btn_Xoa.UseVisualStyleBackColor = true;
+            // 
+            // btn_Them
+            // 
+            this.btn_Them.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btn_Them.Location = new System.Drawing.Point(536, 269);
+            this.btn_Them.Name = "btn_Them";
+            this.btn_Them.Size = new System.Drawing.Size(101, 23);
+            this.btn_Them.TabIndex = 18;
+            this.btn_Them.Text = "THÊM KH";
+            this.btn_Them.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button1.Location = new System.Drawing.Point(898, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 24);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "TÌM KIẾM";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txt_TimKiem
+            // 
+            this.txt_TimKiem.Location = new System.Drawing.Point(778, 198);
+            this.txt_TimKiem.Name = "txt_TimKiem";
+            this.txt_TimKiem.Size = new System.Drawing.Size(100, 22);
+            this.txt_TimKiem.TabIndex = 16;
+            // 
+            // cb_Search
+            // 
+            this.cb_Search.FormattingEnabled = true;
+            this.cb_Search.Location = new System.Drawing.Point(626, 199);
+            this.cb_Search.Name = "cb_Search";
+            this.cb_Search.Size = new System.Drawing.Size(121, 24);
+            this.cb_Search.TabIndex = 15;
+            // 
+            // dt_NgaySinh
+            // 
+            this.dt_NgaySinh.Location = new System.Drawing.Point(685, 47);
+            this.dt_NgaySinh.Name = "dt_NgaySinh";
+            this.dt_NgaySinh.Size = new System.Drawing.Size(200, 22);
+            this.dt_NgaySinh.TabIndex = 14;
+            // 
+            // cb_LoaiKH
+            // 
+            this.cb_LoaiKH.FormattingEnabled = true;
+            this.cb_LoaiKH.Location = new System.Drawing.Point(224, 201);
+            this.cb_LoaiKH.Name = "cb_LoaiKH";
+            this.cb_LoaiKH.Size = new System.Drawing.Size(121, 24);
+            this.cb_LoaiKH.TabIndex = 13;
+            // 
+            // cb_GioiTinh
+            // 
+            this.cb_GioiTinh.FormattingEnabled = true;
+            this.cb_GioiTinh.Location = new System.Drawing.Point(224, 146);
+            this.cb_GioiTinh.Name = "cb_GioiTinh";
+            this.cb_GioiTinh.Size = new System.Drawing.Size(121, 24);
+            this.cb_GioiTinh.TabIndex = 12;
+            // 
+            // txt_DiaChi
+            // 
+            this.txt_DiaChi.Location = new System.Drawing.Point(697, 145);
+            this.txt_DiaChi.Name = "txt_DiaChi";
+            this.txt_DiaChi.Size = new System.Drawing.Size(188, 22);
+            this.txt_DiaChi.TabIndex = 11;
+            // 
+            // txt_SoKH
+            // 
+            this.txt_SoKH.Location = new System.Drawing.Point(727, 102);
+            this.txt_SoKH.Name = "txt_SoKH";
+            this.txt_SoKH.Size = new System.Drawing.Size(158, 22);
+            this.txt_SoKH.TabIndex = 10;
+            // 
+            // txt_TenKH
+            // 
+            this.txt_TenKH.Location = new System.Drawing.Point(224, 102);
+            this.txt_TenKH.Name = "txt_TenKH";
+            this.txt_TenKH.Size = new System.Drawing.Size(193, 22);
+            this.txt_TenKH.TabIndex = 9;
+            // 
+            // txt_IDKH
+            // 
+            this.txt_IDKH.Location = new System.Drawing.Point(224, 49);
+            this.txt_IDKH.Name = "txt_IDKH";
+            this.txt_IDKH.Size = new System.Drawing.Size(193, 22);
+            this.txt_IDKH.TabIndex = 8;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label9.Location = new System.Drawing.Point(535, 195);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 27);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Search";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label8.Location = new System.Drawing.Point(550, 140);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 27);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Địa chỉ";
+            // 
+            // lb_SoKH
+            // 
+            this.lb_SoKH.AutoSize = true;
+            this.lb_SoKH.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lb_SoKH.Location = new System.Drawing.Point(531, 91);
+            this.lb_SoKH.Name = "lb_SoKH";
+            this.lb_SoKH.Size = new System.Drawing.Size(144, 27);
+            this.lb_SoKH.TabIndex = 5;
+            this.lb_SoKH.Text = "Số khách hàng";
+            // 
+            // lb_NgaySinh
+            // 
+            this.lb_NgaySinh.AutoSize = true;
+            this.lb_NgaySinh.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lb_NgaySinh.Location = new System.Drawing.Point(533, 52);
+            this.lb_NgaySinh.Name = "lb_NgaySinh";
+            this.lb_NgaySinh.Size = new System.Drawing.Size(104, 27);
+            this.lb_NgaySinh.TabIndex = 4;
+            this.lb_NgaySinh.Text = "Ngày Sinh";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.Location = new System.Drawing.Point(26, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 27);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Loại khách hàng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(27, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 27);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Giới tính";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 27);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tên Khách Hàng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(27, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 27);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "ID Khách Hàng";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.data_DanhSachKH);
+            this.groupBox2.Location = new System.Drawing.Point(12, 329);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(818, 130);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Danh sách khách hàng ";
+            // 
+            // data_DanhSachKH
+            // 
+            this.data_DanhSachKH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data_DanhSachKH.Location = new System.Drawing.Point(0, 14);
+            this.data_DanhSachKH.Name = "data_DanhSachKH";
+            this.data_DanhSachKH.RowHeadersWidth = 51;
+            this.data_DanhSachKH.RowTemplate.Height = 24;
+            this.data_DanhSachKH.Size = new System.Drawing.Size(1037, 180);
+            this.data_DanhSachKH.TabIndex = 0;
+            // 
+            // QuanLyKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 535);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "QuanLyKhachHang";
+            this.Text = "QuanLyKhachHang";
+            this.Load += new System.EventHandler(this.QuanLyKhachHang_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.data_DanhSachKH)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Button btn_Sua;
+        private System.Windows.Forms.Button btn_Xoa;
+        private System.Windows.Forms.Button btn_Them;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_TimKiem;
+        private System.Windows.Forms.ComboBox cb_Search;
+        private System.Windows.Forms.DateTimePicker dt_NgaySinh;
+        private System.Windows.Forms.ComboBox cb_LoaiKH;
+        private System.Windows.Forms.ComboBox cb_GioiTinh;
+        private System.Windows.Forms.TextBox txt_DiaChi;
+        private System.Windows.Forms.TextBox txt_SoKH;
+        private System.Windows.Forms.TextBox txt_TenKH;
+        private System.Windows.Forms.TextBox txt_IDKH;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lb_SoKH;
+        private System.Windows.Forms.Label lb_NgaySinh;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView data_DanhSachKH;
+    }
+}
---
+++ INDIVIDUAL_PROJECT/QuanLyKhachHang.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INDIVIDUAL_PROJECT
+{
+    public partial class QuanLyKhachHang : Form
+    {
+        public QuanLyKhachHang()
+        {
+            InitializeComponent();
+        }
+
+        private void QuanLyKhachHang_Load(object sender, EventArgs e)
+        {
+            Load_DSKhachHang();
+        }
+        public void Load_DSKhachHang()
+        {
+            String chuoiketnoi = @"";
+            SqlConnection connect = new SqlConnection(chuoiketnoi);
+            connect.Open();
+            string sqlLoad_DSKH = "select * form KHACHHANG";
+            SqlCommand comm= new SqlCommand(sqlLoad_DSKH,connect);
+            SqlDataAdapter adapter = new SqlDataAdapter(comm);
+            DataTable tb=new DataTable();
+            adapter.Fill(tb);
+            data_DanhSachKH.DataSource = tb;
+            connect.Close();
+
+        }
+    }
+}
